80dd776fcf52a20f023a3be8b2730831abfdc01f
edpnetlbk <edpnetlbk@outlook.com>
2024-11-05 03:06:28 +0700

    Update: Seeder Call

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            AdminUsersSeeder::class,
+            ItemSppSeeder::class,
+        ]);
+    }
+}
