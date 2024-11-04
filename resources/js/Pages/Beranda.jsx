6da799b4c2c9e9a82e96bd2c2e73414798795150
edpnetlbk <edpnetlbk@outlook.com>
2024-11-05 03:09:02 +0700

    Fix: Change App Title

--- resources/js/Pages/Beranda.jsx
+++ resources/js/Pages/Beranda.jsx
@@ -12,11 +12,11 @@ export default function Beranda({ auth, laravelVersion, phpVersion }) {
             <GuestLayout>
                 <div className={'w-full h-dvh flex items-center justify-center'}>
                     <div className={'text-white md:max-w-[50%] text-center space-y-10 flex flex-col items-center justify-center'}>
-                        <h1 className={'text-4xl font-semibold'}>Selamat Datang di SIPESED</h1>
-                        <p>SIPESED (Sistem Informasi Pembayaran Siswa Edelweiss) merupakan platform yang berfungsi sebagai media online untuk melakukan pembayaran tagihan PPDB dan SPP Yayasan Edelweiss Kota Cilegon.</p>
-                        <p>Ayo segera bergabung bersama Sekolah Edelweiss dan gunakan platform SIPESED sekarang juga!</p>
+                        <h1 className={'text-4xl font-semibold'}>Selamat Datang di SIPSPP</h1>
+                        <p>SIPSPP (Sistem Pembayaran SPP) merupakan platform yang berfungsi sebagai media online untuk melakukan pembayaran tagihan PPDB dan SPP Yayasan Edelweiss Kota Cilegon.</p>
+                        <p>Ayo segera bergabung bersama Sekolah Edelweiss dan gunakan platform SIPSPP sekarang juga!</p>
                         <CustButton icon={<ArrowCircleRight size={20} />} onClick={() => router.visit(route('login'), { method: 'get' })}>
-                            Masuk SIPESED
+                            Masuk SIPSPP
                         </CustButton>
                     </div>
                 </div>
